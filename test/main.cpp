d7912a7e1e828e17f75d31527f711593e86b2ca3
Lingxi Li <lingxl@microsoft.com>
2023-06-03 12:00:34 +0800

    Test wstr and str logging

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,7 @@
 #include <dbg.hpp>
 
 int main() {
-  dbg::log(123, 3.14);
-  dbg::logline(L"str");
-  dbg::log(L"END");
+    dbg::log(123, 3.14);
+    dbg::logline(L"wstr", "str");
+    dbg::log(L"END");
 }
